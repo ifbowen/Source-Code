d1f75061e611d5965002967ab55534d95928cb1e
bowenzheng <bowenzheng@inke.cn>
2020-05-25 20:11:41 +0800

    修改：runtime版本

---
+++ objc4-781/DebugObjc/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  DebugObjc
+//
+//  Created by Bowen on 2020/5/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSObject *ob = [[NSObject alloc] init];
+    }
+    return 0;
+}
