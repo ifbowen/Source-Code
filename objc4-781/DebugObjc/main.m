2573cd3fd3644edfe7eaeaf7565ba295774f01eb
bowenzheng <bowenzheng@inke.cn>
2020-05-30 19:40:50 +0800

    修改：变动

--- objc4-781/DebugObjc/main.m
+++ objc4-781/DebugObjc/main.m
@@ -10,7 +10,17 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        [Person run];
+//        [Person run];
+        
+        // xcrun -sdk iphoneos clang -arch arm64 -rewrite-objc -fobjc-arc -fobjc-runtime=ios-13.2 main.m
+        // objc_initWeak
+        Person *p = [[Person alloc] init];
+        __weak Person *pp = p;
+        __weak Person *ppp = p;
+        __weak Person *pppp = p;
+        __weak Person *ppppp = p;
+        __weak Person *pppppp = p;
+        NSLog(@"%@", pp);
     }
     return 0;
 }
